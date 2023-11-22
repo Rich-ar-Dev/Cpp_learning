16c39ff4611f7d71879ad21c73212e5387a5ce65
Rich-ar-Dev <moriasrichard3@gmail.com>
2023-11-22 10:34:38 -0800

    memorization

---
+++ Memorization.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+// Function to calculate factorial using recursion
+unsigned long long calculateFactorial(int n)
+{
+  unsigned long long factorial = 1;
+
+  // Base case: If n is 0, return 1
+  if (n == 0)
+  {
+    return 1;
+  }
+  else
+  {
+    // Recursive case: Calculate factorial recursively
+    for ( int i= 2; i<=n; ++i) {
+      factorial = factorial * i;
+    }
+    return factorial;
+  }
+}
+int main()
+{
+  // Declare a variable to store the input number
+  int n;
+
+  // Prompt the user to enter a non-negative integer
+  std::cout << "Enter a non-negative integer to calculate its factorial: ";
+  std::cin >> n;
+
+  // Check if the input is valid (non-negative)
+  if (n < 0)
+  {
+    // Display an error message for invalid input
+    std::cout << "Invalid input. Please enter a non-negative integer." << std::endl;
+    // Exit with an error code
+    return 1;
+  }
+
+  // Calculate factorial using recursion
+  unsigned long long result = calculateFactorial(n);
+
+  // Display the calculated factorial
+  std::cout << "Factorial of " << n << " = " << result << std::endl;
+
+  // Exit successfully
+  return 0;
+}
